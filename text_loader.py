ffed5c37386e172f0f41bf75f67657ad222df70b
AS2-250c1ee6c254696c <tasfiq.a@gmail.com>
2024-04-30 18:18:34 +0000

    add text chunking

--- text_loader.py
+++ text_loader.py
@@ -1,31 +1,51 @@
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
 import os 
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import re
+from typing import List
+import embeddings
 
-def load_text(file_path):
-    loader = TextLoader(file_path, autodetect_encoding = True)
-    doc = loader.load()
-    return doc
 
-def split_text(document): 
-    text_splitter = CharacterTextSplitter(
-    separator="\n\n",
-    chunk_size=800,
-    chunk_overlap=80,
+def load_text(base_dir):
+    text_loader_kwargs={'autodetect_encoding': True}
+    loader = DirectoryLoader(base_dir, glob="**/*.txt", loader_cls=TextLoader, loader_kwargs=text_loader_kwargs)
+    return [doc.page_content for doc in loader.load()]
+
+
+def clean_text(text_list): 
+    def _clean_text(text): 
+        file_contents =  text.rstrip('/r/n/0/t/f/v')
+        pattern = r'\s+'
+        return re.sub(pattern, ' ', file_contents)
+        
+    return [_clean_text(text) for text in text_list]
+
+
+def split_text(text_list): 
+    separators=[
+        "\n\n",
+        "\n",
+        " ",
+        ".",
+        ",",
+    ]
+
+    text_splitter = RecursiveCharacterTextSplitter(
+    separators=separators, 
+    chunk_size=1000,
+    chunk_overlap=50,
     length_function=len,
     is_separator_regex=False,
 )
-    text = document.page_content
-    return text_splitter.create_documents([text])
+    return text_splitter.create_documents(text_list)
+
     
 if __name__ == "__main__": 
     base_dir = r"data\\full_contract_txt"
-    text_files = os.listdir(r"data\\full_contract_txt")
-    sample_text_file = text_files[0]
-    sample_file_path = os.path.join(base_dir, sample_text_file)
-    docs = load_text(sample_file_path)
+    text_list = load_text(base_dir)
+    cleaned_text_list = clean_text(text_list)
+    split_texts = split_text(cleaned_text_list)
+    
 
-    chunks = split_text(docs[0])
-
-    print(len(chunks))
+    
     
