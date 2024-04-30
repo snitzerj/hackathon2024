8de5df203071ad4a53ffda2457a952a4657c9bca
Saul Lopez Valdez <saullopezvaldez@gmail.com>
2024-04-30 19:12:18 +0000

    changes

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from llama_index.query_engine import PandasQueryEngine
-from prompts import new_prompt, instruction_str, context
-from note_engine import note_engine
-from llama_index.tools import QueryEngineTool, ToolMetadata
-from llama_index.agent import ReActAgent
-from llama_index.llms import OpenAI
-from pdf import canada_engine
+from llama_index.core.query_engine import PandasQueryEngine
+from prompt import new_prompt, instruction_str, context
+# from note_engine import note_engine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+from pdf import pdf_engine
 
 load_dotenv()
+
+
+tools = [
+    QueryEngineTool(
+        query_engine=pdf_engine,
+        metadata=ToolMetadata(
+            name="contract_full_content_data",
+            description="this gives detailed information about contract text content",
+        ),
+    ),
+]
+
+llm = OpenAI(model="gpt-3.5-turbo-0613")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
---
+++ pdf.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
+from llama_index.core.readers import SimpleDirectoryReader
+load_dotenv()
+
+def get_index(data, index_name):
+    index = None
+    if not os.path.exists(index_name):
+        print("building index", index_name)
+        index = VectorStoreIndex.from_documents(data, show_progress=True)
+        index.storage_context.persist(persist_dir=index_name)
+    else:
+        index = load_index_from_storage(
+            StorageContext.from_defaults(persist_dir=index_name)
+        )
+
+    return index
+
+
+pdf_path = os.path.join("Contract_Data", "Supply")
+reader = SimpleDirectoryReader(input_dir = pdf_path)
+data = reader.load_data()
+pdf_index = get_index(data, "contract_pdf_vector_data")
+pdf_engine = pdf_index.as_query_engine()
