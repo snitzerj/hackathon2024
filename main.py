eeea56458d42f616073c5ffdd3b00fe9b8d91aaa
Saul Lopez Valdez <saullopezvaldez@gmail.com>
2024-04-30 17:04:21 +0000

    changes

---
+++ main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str, context
+from note_engine import note_engine
+from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.agent import ReActAgent
+from llama_index.llms import OpenAI
+from pdf import canada_engine
+
+load_dotenv()
